a082cf2e1bdd31d78ae59f2d8547f273a2ead0ab
chrisnatali <chris.natali@gmail.com>
2015-10-27 12:42:26 -0400

    default config

--- sync_load_cfg.rb
+++ sync_load_cfg.rb
@@ -1,4 +1,4 @@
-SYNC_LOAD_API_URL="192.168.33.85:3000"
+SYNC_LOAD_API_URL="0.0.0.0:3000" # change me to point to osm server url
 SYNC_LOAD_SYNC_DIR="/home/tiles/sync_load"
 SYNC_LOAD_POSTGIS_DB="osm_grid"
-SYNC_LOAD_STYLE_FILE="/home/tiles/gridmaps_pgsql.style"
+SYNC_LOAD_STYLE_FILE="/home/tiles/gridmaps.style"
